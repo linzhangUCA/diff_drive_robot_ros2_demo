7e4b320d9dc856cd44e9349aeb9240205ee85a15
linzhanguca <lzhang12@ucae.edu>
2024-03-04 16:00:40 -0600

    update package related info

--- robot_gazebo/setup.py
+++ robot_gazebo/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'robot_gazebo'
@@ -10,12 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "launch"), glob(os.path.join("launch", "*"))),
+        (os.path.join("share", package_name, "worlds"), glob(os.path.join("worlds", "*"))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='pbd0',
+    maintainer='linzhangUCA',
     maintainer_email='lzhang12@ucae.edu',
-    description='TODO: Package description',
+    description='Demo robot simulation package',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
