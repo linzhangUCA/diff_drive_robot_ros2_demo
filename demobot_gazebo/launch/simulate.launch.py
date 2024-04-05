a92b227ff567f08984e6f4467af50966d063bdf3
linzhanguca <lzhang12@uca.edu>
2024-04-05 14:03:56 -0500

    add diff drive plugin in urdf

---
+++ demobot_gazebo/launch/simulate.launch.py
@@ -0,0 +1,77 @@
+from xmlrpc.client import boolean
+from ament_index_python.packages import get_package_share_path
+import launch
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    urdf_package_path = get_package_share_path("demobot_description")
+    model_path = urdf_package_path / "urdf/demobot.urdf.xacro"
+
+    # Launch arguments
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=str(model_path),
+        description="Absolute path to robot urdf file",
+    )
+    sim_time_arg = DeclareLaunchArgument(
+        name="use_sim_time",
+        default_value="true",
+        choices=["true", "false"],
+        description="Flag to enable use simulation time",
+    )
+
+    robot_description = ParameterValue(
+        Command(["xacro ", LaunchConfiguration("model")]), value_type=str
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {
+                "use_sim_time": LaunchConfiguration("use_sim_time"),
+                "robot_description": robot_description,
+            }
+        ],
+    )
+    gazebo_process = ExecuteProcess(
+        cmd=[
+            "gazebo",
+            "--verbose",
+            "-s",
+            "libgazebo_ros_init.so",
+            "-s",
+            "libgazebo_ros_factory.so",
+        ],
+        output="screen",
+    )
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-entity",
+            "demobot",
+            "-topic",
+            "robot_description",
+            "-x",
+            "0.0",
+            "-y",
+            "0.0",
+            "-z",
+            "0.0",
+        ],
+        output="screen",
+    )
+
+    return launch.LaunchDescription(
+        [
+            sim_time_arg,
+            model_arg,
+            robot_state_publisher_node,
+            gazebo_process,
+            spawn_entity,
+        ]
+    )
