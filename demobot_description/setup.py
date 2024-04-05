ee452bd50eb096b3ddd38b20177fcf08889c0a5b
linzhanguca <lzhang12@uca.edu>
2024-04-05 13:19:45 -0500

    update urdf

--- demobot_description/setup.py
+++ demobot_description/setup.py
@@ -2,7 +2,7 @@ import os
 from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'robot_description'
+package_name = 'demobot_description'
 
 setup(
     name=package_name,
@@ -12,15 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.xacro'))),
-        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='linzhangUCA',
-    maintainer_email='lzhang12@ucae.edu',
-    description='A fictional diff-drive robot model desription',
+    maintainer='pbd0',
+    maintainer_email='lzhang12@uca.edu',
+    description='Package contains most basic urdf for a diff-drive robot',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
