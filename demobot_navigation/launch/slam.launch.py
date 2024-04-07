9b26b8940eb8bf2ae4009f49577e273e98a77f6f
linzhanguca <lzhang12@ucae.edu>
2024-04-07 11:38:59 -0500

    update branch with new package names

---
+++ demobot_navigation/launch/slam.launch.py
@@ -0,0 +1,53 @@
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    urdf_package_path = get_package_share_path('demobot_description')
+    gazebo_package_path = get_package_share_path('demobot_gazebo')
+    rviz_config_path = urdf_package_path / 'rviz/demobot.rviz'
+
+    sim_time_arg = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        choices=['true', 'false'],
+        description="Flag to enable use simulation time",
+    )
+    rviz_arg = DeclareLaunchArgument(
+        name='rvizconfig',
+        default_value=str(rviz_config_path),
+        description="Absolute path to rviz config file",
+    )
+
+    launch_simulation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            str(gazebo_package_path / 'launch/simulate.launch.py')
+        )
+    )
+    launch_online_async_slam = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            str(get_package_share_path('slam_toolbox') / 'launch/online_async_launch.py')
+        )
+    )
+    
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return LaunchDescription(
+        [
+            sim_time_arg,
+            rviz_arg,
+            launch_simulation,
+            launch_online_async_slam,
+            rviz_node,
+        ]
+    )
